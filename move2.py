7a7f8cb5e42824876e57d2372e336673863051bc
swampsully <sylvestersolomon02@gmail.com>
2025-05-16 02:19:25 +0100

    I finally got the motors to work with the ontroller

--- move2.py
+++ move2.py
@@ -4,34 +4,24 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 current_pos = 0
-new_pos = 0
-value2 = 0
 
 def move_servo():
     global current_pos
-    global value2
-    global new_pos
-    if new_pos >= 180:
-        new_pos = 180
-        print("TOO FAR BITCH BOI")
-    if new_pos<= 0:
-        new_pos = 0
-    new_pos = value2 - current_pos 
-    kit.servo[2].angle = (new_pos)
+    kit.servo[2].angle = (current_pos)
 
 def turn_right():
-   value2 = current_pos + 10
-   if value2 >=180:
-       value2 = 180
-   print(value2)
+   global current_pos
+   value1 = (current_pos + 10)
+   if value1 >=180:
+       value1 = 180
+   current_pos = value1
 
 def turn_left():
-   value1 = current_pos
-   value2 = value1 - 10
+   global current_pos
+   value1 = (current_pos - 10)
    if value1 <=0:
-       value2 = 0
-   print(value2)
-
+       value1 = 0
+   current_pos = value1
 
 class MyController(Controller):
     def __init__(self, **kwargs):
@@ -39,15 +29,20 @@ class MyController(Controller):
 
        # This exits the program #
     def on_playstation_button_press(self):
-            os.system("quit")
+            usr = str(input("Are you sure you are done?[yes or no]  "))
+            if usr == "yes":
+                print("You're a Bitch!")
+                os.system(quit)
+            else:
+                pass
 
             # This block is to controls the base axis #
     def on_right_arrow_press(self):
-           turn_right() 
+        turn_right() move_servo()
     def on_left_right_arrow_release(self):
-           move_servo() 
+        #move_servo() 
     def on_left_arrow_press(self):
-          turn_left() 
+        turn_left() 
 
         # This block controls the first tilt axis #
     def on_up_arrow_press(self): 
@@ -70,7 +65,5 @@ class MyController(Controller):
             kit.continuous_servo[0].throttle = 0
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.debug = True
-
 controller.listen(timeout=60)
 
